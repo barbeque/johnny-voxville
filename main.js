898bb40000bed958f8e8566e05dd9a6df3700b6a
mike stedman <github@ravuya.com>
2014-12-11 20:59:36 -0700

    world generation

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-var vx = {
-  step: function(state) {
-    // blot out the screen
-    state.context.clearRect(0, 0, state.canvas.width, state.canvas.height);
-    // draw the voxel world
-    state.context.beginPath();
-    state.context.moveTo(100, 150);
-    state.context.lineTo(450, 50);
-    state.context.stroke();
-    // get input
-    // do whatever updates we need
-  },
+var vx = vx || {};
+vx.step = function(state) {
+  // blot out the screen
+  state.context.clearRect(0, 0, state.canvas.width, state.canvas.height);
+  // draw the voxel world
+  state.context.beginPath();
+  state.context.moveTo(100, 150);
+  state.context.lineTo(450, 50);
+  state.context.stroke();
+  // get input
+  // do whatever updates we need
+};
 
-  /// launcher function
-  go: function() {
-    state = {};
-    state.canvas = document.getElementById('theatre');
-    state.context = state.canvas.getContext('2d');
-    var stepThunk = function(s) {
-      return function() { vx.step(s); }
-    }(state);
-    setInterval(stepThunk, 100);
-  }
-}
+/// launcher function
+vx.go = function() {
+  state = {};
+  state.canvas = document.getElementById('theatre');
+  state.context = state.canvas.getContext('2d');
+  state.world = vx.generateCubeWorld(100, 100, 100);
+  var stepThunk = function(s) {
+    return function() { vx.step(s); }
+  }(state);
+  setInterval(stepThunk, 100);
+};
 
 window.onload = window.onresize = function() {
   canvas = document.getElementById('theatre');
---
+++ voxworld.js
@@ -0,0 +1,26 @@
+var vx = vx || {};
+
+vx.makeCubeWorldArray = function(width, height, depth) {
+  var output = new Array(depth);
+  for(var z = 0; z < depth; ++z) {
+    var slice = new Array(height);
+    for(var y = 0; y < height; ++y) {
+      slice[y] = new Array(width);
+    }
+    output[z] = slice;
+  }
+  return output;
+};
+
+vx.generateCubeWorld = function(width, height, depth) {
+  // TODO: seeded random
+  var box = vx.makeCubeWorldArray(width, height, depth);
+  for(var z = 0; z < depth; ++z) {
+    for(var y = 0; y < height; ++y) {
+      for(var x = 0; x < width; ++x) {
+        box[z][y][x] = Math.floor(Math.random() * 10);
+      }
+    }
+  }
+  return box;
+};
